c5220c329ede33cd289bbebfd4a683e1ce7c5f24
Will Gleason <wgleason9@gmail.com>
2022-11-18 12:48:40 -0800

    1:32, user login

--- fiddling/src/resolvers/users.ts
+++ fiddling/src/resolvers/users.ts
@@ -1,29 +1,87 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Resolver, Mutation, Arg, InputType, Field, Ctx } from "type-graphql"; 
-import argon2 from 'argon2';
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  InputType,
+  Field,
+  Ctx,
+  ObjectType,
+} from "type-graphql";
+import argon2 from "argon2";
 
 @InputType()
 class UserNamePasswordInput {
-    @Field()
-    username: string;
-    @Field()
-    password: string;
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class Error {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [Error], { nullable: true })
+  errors?: Error[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
-    async register(
-        @Arg('options') options: UserNamePasswordInput,
-        @Ctx() { em }: MyContext
-    ){
-        const hashedPassword = await argon2.hash(options.password);
-        const user = em.create(User, { 
-            username: options.username, 
-            password: hashedPassword 
-        });
-        em.persistAndFlush(user);
-        return user;
+  @Mutation(() => User)
+  async register(
+    @Arg("options") options: UserNamePasswordInput,
+    @Ctx() { em }: MyContext
+  ) {
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    await em.persistAndFlush(user);
+    return user;
+  }
+
+  @Mutation(() => UserResponse, { nullable: true })
+  async login(
+    @Arg("options") options: UserNamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "that username does not exist",
+          },
+        ],
+      };
     }
+    const valid = await argon2.verify(user.password, options.password);
+    if (valid) {
+      return {
+        user,
+      };
+    }
+    return {
+      errors: [
+        {
+          field: "password",
+          message: "incorrect password",
+        }
+      ],
+    };
+  }
 }
